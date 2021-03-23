d4c4a9bd50f3c21adec97b0a4b47989e657caa23
Jithendra <jithendrads@gmail.com>
2021-03-23 20:50:30 +0530

    Child Thread creation by extending Thread class

---
+++ src/ThreadDemo.java
@@ -0,0 +1,26 @@
+class MyThread extends Thread
+{
+	public void run()
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("child thread");
+		}
+	}
+}
+public class ThreadDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MyThread t=new MyThread();
+		t.start();
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Main Thread");
+		}
+
+	}
+
+}
